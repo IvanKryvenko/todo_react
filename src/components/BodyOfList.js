40a981aea61af62532aec70a4421b51286709401
Ivan Kryvenko <ivan.kryv14@gmail.com>
2020-10-21 13:44:11 +0300

    Added new functionality

--- src/components/BodyOfList.js
+++ src/components/BodyOfList.js
@@ -9,12 +9,35 @@ function BodyOfList(props) {
     const [taskArray, setTask] = useState([]);
 
     let onArrayChange = (task) => {
-        setTask([...taskArray, task]);
+        setTask([task, ...taskArray]);
     }
 
+    let onTaskDelete = (id) => {
+        console.log(id);
+        let newArray = taskArray.filter((item) => item.id !== id);
+        setTask(newArray);
+    }
+
+    let onTaskEdit = (id, text) => {
+        let arrayCopy = taskArray;
+        let editedTaskIndex = arrayCopy.findIndex(item => item.id === id);
+        arrayCopy[editedTaskIndex].text = text;
+        setTask(arrayCopy);
+        // editedTask.text = text;
+    }
+
+    let onComplite = (id) => {
+        let arrayCopy = taskArray;
+        let complitedTaskIndex = arrayCopy.findIndex(item => item.id === id);
+        arrayCopy[complitedTaskIndex].isComplete = !arrayCopy[complitedTaskIndex].isComplete;
+        setTask(arrayCopy);
+    }
+
+
     let taskList = taskArray.map((element) => 
-        <Task text={element.text} id={element.id} key={element.id} state={element.isComplete} />
-    );
+        <Task text={element.text} id={element.id} key={element.id} state={element.isComplete} onDelete={onTaskDelete} onEdit={onTaskEdit} complete={onComplite} />
+    ); 
+ 
 
     return (
         <content>
@@ -27,6 +50,8 @@ function BodyOfList(props) {
                     array={taskArray}
                     onAddingTask={onArrayChange} />
                     {taskList}
+                    <button>Completed Tasks</button>
+                    <button>All Tasks</button>
                 </div>
                 <div className="col-xl-3 col-lg-3 col-md-2">
 
--- src/components/Task.component.js
+++ src/components/Task.component.js
@@ -3,14 +3,47 @@ import React, { useState } from 'react';
 let Task = (props) => {
 
     let [isComplete, onComplete] = useState(props.state);
+    let [itEditing, onEdit] = useState(false);
+    let [text, setText] = useState(props.text);
 
-    return (
-        <div id={props.id} className="task" style={!isComplete ? {backgroundColor: '#FFB079'} : {backgroundColor: '#D9FE85'}}>
-            <p>{props.text}</p>
-            <button className="task-button" onClick={() => onComplete(!isComplete)}>Complete</button>
-            <button className="task-button">Delete</button>
-        </div>
-    )
+    let deleteTask = (id) => {
+        props.onDelete(id);
+    }
+
+    let sendEditTask = (id, text) => {
+        props.onEdit(id, text)
+    }
+
+    let taskEdit = (id, text) => {
+        onEdit(!itEditing);
+        itEditing ? sendEditTask(id, text) : console.log("Done!");
+    }
+
+    let complete = (id) => {
+        props.complete(id);
+        onComplete(!isComplete);
+    }
+
+
+    if(!itEditing) {
+        return (
+            <div id={props.id} className="task" style={!isComplete ? {backgroundColor: '#FFB079'} : {backgroundColor: '#D9FE85'}}>
+                <p>{text}</p>
+                <button className="task-button" onClick={() => complete(props.id)}>Complete</button>
+                <button className="task-button" onClick={() => deleteTask(props.id)}>Delete</button>
+                <button className="task-button" onClick={() => taskEdit(props.id, text)}>Edit</button>
+            </div>  
+        )
+    } else {
+        return (
+            <div id={props.id} className="task" style={!isComplete ? {backgroundColor: '#FFB079'} : {backgroundColor: '#D9FE85'}}>
+                <input className="input-task-text" type="text" value={text} onChange={(e) => setText(e.target.value)} />
+                <button className="task-button" onClick={() => complete(props.id)}>Complete</button>
+                <button className="task-button" onClick={() => deleteTask(props.id)}>Delete</button>
+                <button className="task-button" onClick={() => taskEdit(props.id, text)}>Done</button>
+            </div>
+        )
+    }  
 }
 
 export default Task;
